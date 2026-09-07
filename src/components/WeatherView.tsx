a5212e943b9342f9fd0cfe63d9965c42e6de8f4a
theyapguard <theyapguard@gmail.com>
2026-09-08 02:57:02 +0500

    Preceding changes

--- src/components/WeatherView.tsx
+++ src/components/WeatherView.tsx
@@ -465,7 +465,7 @@ const WeatherView = ({ activeField, selectedFields }: WeatherViewProps) => {
                   <CartesianGrid strokeDasharray="3 3" stroke="hsl(150, 12%, 22%)" />
                   <XAxis dataKey="month" stroke="hsl(150, 10%, 55%)" fontSize={11} />
                   <YAxis stroke="hsl(150, 10%, 55%)" fontSize={11} />
-                  <Tooltip contentStyle={tooltipStyle} />
+                  <Tooltip content={<CustomChartTooltip />} />
                   <Area type="monotone" dataKey="accumulated" stroke={CHART_GOLD} strokeWidth={2.5} fill="url(#goldGrad)" dot={{ r: 3, fill: CHART_GOLD }} activeDot={{ r: 5 }} />
                   <Line type="monotone" dataKey="evapotranspiration" stroke={CHART_CREAM} strokeWidth={1.5} dot={false} strokeDasharray="5 5" />
                 </AreaChart>
